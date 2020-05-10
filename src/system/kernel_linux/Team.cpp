8f97c85080bfca051861de14feefbe70fbaf1e8c
Ivan Gualandri <dreamos82@yahoo.it>
2020-05-10 19:52:32 +0100

    Add argc and args to _kern_get_team_info

--- src/system/kernel_linux/Team.cpp
+++ src/system/kernel_linux/Team.cpp
@@ -6,12 +6,15 @@
 #include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
 
 #include "syscalls.h"
 
 #define UID_LINE 8
 #define GID_LINE 9
-
+#define MAX_BYTES 64
+#define MAX_LINE_LENGTH 1024
 status_t
 _kern_get_team_usage_info(team_id team, int32 who, team_usage_info *info, size_t size)
 {
@@ -35,42 +38,45 @@ status_t
 _kern_get_team_info(team_id id, team_info *info)
 {
   	char proc_path[500];
-    char buffer[1024];
+    char buffer[MAX_LINE_LENGTH];
     char command_proc_path[500];
     sprintf(proc_path, "/proc/%d/status", id);
     sprintf(command_proc_path, "/proc/%d/cmdline", id);
   	FILE *fileid = fopen(proc_path, "r");
-    FILE *file_command_id = fopen(command_proc_path, "r");
+    int file_command_id = open(command_proc_path, O_RDONLY);
     if(fileid == NULL) {
 		return B_ERROR;
 	}
     int i=0;
     while(!feof(fileid)){
-        fgets(buffer, 1024, fileid);
+        fgets(buffer, MAX_LINE_LENGTH, fileid);
         if( i == UID_LINE) {
             sscanf(buffer, "Uid:\t%u\t", &info->uid);
         } else if( i == GID_LINE) {
             sscanf(buffer, "Gid:\t%u\t", &info->gid);
         } else {
-            if(strncmp(buffer, "Threads:", sizeof("Threads")) == 0){
+            if(strncmp(buffer, "Threads:", sizeof("Threads:")) == 0){
                 sscanf(buffer, "Threads:\t%d", &info->thread_count);
                 break;
             }
         }
         i++;
-        //fread(buffer, sizeof(char), 1025, fileid) > 0){
     }
-    printf("---\n");
-    fgets(buffer, 1024, file_command_id);
-    char *token = strtok(buffer, " ");
+    int bytes_read = read(file_command_id, buffer, MAX_LINE_LENGTH);
     int argCount = 0;
-    while(strtok(NULL, " ") != NULL){
-        argCount++;
+    int j = 0;
+    while(j < bytes_read){
+        if(buffer[j] == '\0'){
+            argCount++;
+            buffer[j] = ' ';
+        }
+        j++;
     }
+    buffer[j] = '\0';
     info->argc = argCount;
-    strncpy(info->args, buffer, 64);
+    strncpy(info->args, buffer, MAX_BYTES);
     fclose(fileid);
-    fclose(file_command_id);
+    close(file_command_id);
     return B_OK;
 }
 
