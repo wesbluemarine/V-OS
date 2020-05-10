3490548e14797dfcd43d72dc5f545e3d33989ef7
Ivan Gualandri <dreamos82@yahoo.it>
2020-05-10 11:55:15 +0100

    Implement draft of kern_get_team_info

--- src/system/kernel_linux/Team.cpp
+++ src/system/kernel_linux/Team.cpp
@@ -5,9 +5,12 @@
 
 #include <errno.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 #include "syscalls.h"
 
+#define UID_LINE 8
+#define GID_LINE 9
 
 status_t
 _kern_get_team_usage_info(team_id team, int32 who, team_usage_info *info, size_t size)
@@ -31,13 +34,44 @@ _kern_kill_team(team_id team)
 status_t
 _kern_get_team_info(team_id id, team_info *info)
 {
-	char proc_path[500];
-	info->team = id;
-	printf("id: %d\n", id);
-	sprintf(proc_path, "/proc/%d", id);
-	printf(proc_path);
-	printf("\n");
-	return B_OK;
+  	char proc_path[500];
+    char buffer[1024];
+    char command_proc_path[500];
+    sprintf(proc_path, "/proc/%d/status", id);
+    sprintf(command_proc_path, "/proc/%d/cmdline", id);
+  	FILE *fileid = fopen(proc_path, "r");
+    FILE *file_command_id = fopen(command_proc_path, "r");
+    if(fileid == NULL) {
+		return B_ERROR;
+	}
+    int i=0;
+    while(!feof(fileid)){
+        fgets(buffer, 1024, fileid);
+        if( i == UID_LINE) {
+            sscanf(buffer, "Uid:\t%u\t", &info->uid);
+        } else if( i == GID_LINE) {
+            sscanf(buffer, "Gid:\t%u\t", &info->gid);
+        } else {
+            if(strncmp(buffer, "Threads:", sizeof("Threads")) == 0){
+                sscanf(buffer, "Threads:\t%d", &info->thread_count);
+                break;
+            }
+        }
+        i++;
+        //fread(buffer, sizeof(char), 1025, fileid) > 0){
+    }
+    printf("---\n");
+    fgets(buffer, 1024, file_command_id);
+    char *token = strtok(buffer, " ");
+    int argCount = 0;
+    while(strtok(NULL, " ") != NULL){
+        argCount++;
+    }
+    info->argc = argCount;
+    strncpy(info->args, buffer, 64);
+    fclose(fileid);
+    fclose(file_command_id);
+    return B_OK;
 }
 
 
